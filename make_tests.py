06161632bd91b46622bc2de456afc3dedd0e8001
Michael Bolt <mike@invalid.org>
2023-11-20 09:13:56 +0000

    Something isn't right
    Either in the tests or in the calculations... That's why we do this.

--- make_tests.py
+++ make_tests.py
@@ -1,14 +1,17 @@
 import configparser
-from tracking import deg2rad
+from tracking import deg2rad, mm_to_angle, CORRECT_TOE
 from math import *
 from pdb import set_trace as brk
 
 
-# Create some deliberately inaccurate measurements from measurements.ini to
-# check our results
+# Create some deliberately inaccurate measurements to check our calculations
 
 def add_error(config, fname, toe):
-	at = atan(toe)
+	actual_front_toe = CORRECT_TOE
+	actual_rear_toe = mm_to_angle(3.0)
+	track = 1478.41
+
+	t = tan(toe)
 	with open(fname, "w") as fp:
 		for section_name in config.sections():
 			print("[{}]".format(section_name), file=fp)
@@ -16,8 +19,8 @@ def add_error(config, fname, toe):
 			print("distance = {}".format(section["distance"]), file=fp)
 			distance = float(section["distance"])
 
-			front = float(section["front"]) - at * distance
-			rear = float(section["rear"]) - at * distance
+			front = track - tan(actual_front_toe) * distance
+			rear = track - tan(actual_rear_toe) * distance
 
 			print("front = {}".format(front), file=fp)
 			print("rear = {}\n".format(rear), file=fp)
@@ -30,7 +33,7 @@ def main():
 	config.read("measurements.ini")
 
 	# Simulate measurements with a tool that systematically toes in
-	add_error(config, "toed_in.ini", deg2rad(0.5))
+	add_error(config, "toed_in.ini", deg2rad(0))
 
 	# Simulate measurements with a tool that systematically toes out
 	add_error(config, "toed_out.ini", deg2rad(-0.5))
